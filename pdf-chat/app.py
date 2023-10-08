70b2c2c44cdeec522f67cad791598a09be9bb830
naresh-sabapathy92 <reshs92.09@gmail.com>
2023-10-08 22:49:58 +0800

    app.py: Added pdf chat logic

--- pdf-chat/app.py
+++ pdf-chat/app.py
@@ -1,3 +1,15 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
+import os
+import pickle
+
+from dotenv import load_dotenv
+
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 
@@ -20,6 +32,8 @@ with st.sidebar:
 def main():
   st.header( 'Chat with PDF' )
   
+  load_dotenv()
+
   pdf = st.file_uploader( 
     'Upload a PDF file', 
     type='pdf' 
@@ -27,7 +41,57 @@ def main():
 
   if pdf is not None:
     pdf_reader = PdfReader( pdf )
-    st.write( pdf_reader )
+
+    text = ''
+    for page in pdf_reader.pages:
+      text += page.extract_text()
+    
+    text_splitter = RecursiveCharacterTextSplitter( 
+      chunk_size=1000,
+      chunk_overlap=200,
+      length_function=len
+    )
+
+    chunks = text_splitter.split_text( text = text )
+
+    store_name = pdf.name[ : -4 ]
+
+    if os.path.exists( f'{store_name}.pkl' ):
+      with open( f'{store_name}.pkl', 'rb' ) as f:
+        vector_store = pickle.load( f )
+    
+    else:
+      embedding = OpenAIEmbeddings()
+      vector_store = FAISS.from_texts(
+        chunks,
+        embedding = embedding
+      )
+
+      with open( f'{store_name}.pkl', 'wb' ) as f:
+        pickle.dump( vector_store, f )
+
+    query = st.text_input( 'Ask a question about the PDF' )
+
+    if query:
+      docs = vector_store.similarity_search( query = query )
+      
+      llm = OpenAI(
+        temperature = 0
+      )
+
+      chain = load_qa_chain(
+        llm = llm,
+        chain_type='stuff'
+      )
+
+      with get_openai_callback() as cb:
+        response = chain.run(
+          input_documents = docs,
+          question = query
+        )
+        print( cb )
+
+      st.write( response )
 
 if __name__ == '__main__':
   main()
